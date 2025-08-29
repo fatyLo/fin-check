3025101b41c9ed2f34579860f946706113a39e9a
faty <fatylo90@gmail.com>
2025-08-29 12:53:27 +0200

    Initial commit

---
+++ src/main/java/com/example/fincheck/FinCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.fincheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fincheck/FinCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fincheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
